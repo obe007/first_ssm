dea2b2aab4fedff00d5021f0b3e91f73bcda47b4
Administrator <275152725@163.com>
2018-06-09 11:11:25 +0800

    Initial commit

---
+++ src/main/resources/mapper/CategoryMapper.java
@@ -0,0 +1,23 @@
+//package com.ssn.mapper;
+//
+//import java.util.List;
+//
+//import com.ssm.pojo.Category;
+//
+//public interface CategoryMapper {
+//
+//
+//    public int add(Category category);
+//
+//    public void delete(int id);
+//
+//    public Category get(int id);
+//
+//    public int update(Category category);
+//
+//    public List<Category> list();
+//
+//    public int count();
+//
+//
+//}
